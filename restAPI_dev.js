ac97d3d4ceee3bd6e6701595ee61c704f95d99da
gracelamalva <gracevlamalva@gmail.com>
2020-12-06 23:51:06 -0500

    fix

--- restAPI_dev.js
+++ restAPI_dev.js
@@ -1,6 +1,7 @@
 //npm install express in terminal
 var express = require ('express');
 var app = express();
+
 var port = process.env.PORT || 3000;
 
 app.set('views', __dirname + '/demo/front_end');
@@ -8,15 +9,14 @@ app.engine('html', require('ejs').renderFile);
 
 var api_routes = require('./api_routes_dev.js');
 app.use('/api', api_routes)
-app.get('/', (req, res) => {
 
+app.get('/', (req, res) => {
     res.render("index_dev.html");
-     
  })
 
 app.use('/demo', express.static('demo/front_end'));
 
 app.listen(port, function() {
-    console.log('Server is running on port' + port);
+    console.log('Server is running on port ' + port);
    
 })
